4c697657f2ec536e2f774767d0f123b7579cd6df
Sojjwal Kelkar <s.kelkar@maxxton.com>
2015-10-24 14:47:51 +0530

    init commit

---
+++ src/main/java/com/maxxton/mis/leave/MisLeaveServiceApplication.java
@@ -0,0 +1,12 @@
+package com.maxxton.mis.leave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisLeaveServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MisLeaveServiceApplication.class, args);
+    }
+}
